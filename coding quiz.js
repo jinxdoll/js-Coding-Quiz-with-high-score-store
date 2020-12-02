6512602c7b2cf1e297601e57b2e8808a0bd1f822
Aimee Mattson <73260853+jinxdoll@users.noreply.github.com>
2020-12-02 15:08:00 -0800

    Add files via upload
    Adding working files to repository

---
+++ coding quiz.js
@@ -0,0 +1,53 @@
+// var questions = [
+//     { q: "Javascript is a programming language.", a: "True", b: ""},
+//     { q: "HTML stnads for hot tamale meat loaf.", a: "False"},
+//     { q: "Javascript is also known as 'JS'.",     a: "True"},
+//     { q: "Metadata goes in the <footer> section.", a: "False"},
+// ];
+
+// var startButton = document.getElementById("startButton")
+// // container for button needed.
+// var questionAButton = document.createElement("button")
+// var questionBButton = document.createElement("button")
+// var questionCButton = document.createElement("button")
+// var questionDButton = document.createElement("button")
+// questionAButton.classList="answerButtonClass"
+
+// var questionArray = []
+// var currentQuestion 
+
+// var i = 0;
+
+// var score = 0;
+
+// function startQuiz() {
+//     for (var i =0; i <questions.length; i++) {
+//         var answer = confirm(questions[i].q);
+    
+//         if (answer === true && questions[i].a === "true" ||
+//         answer === false && questions[i].a === "false") {
+    
+//             score++;
+//             alert("You are correct!");            
+//         } 
+//         else {
+//             alert("You are wrong!");
+//         }  
+//     }
+// }
+
+
+// function questionRenderer() {
+// if(questionsArray.lenght >=questions.length) {
+//     // end quiz function here
+// }
+// else{
+//     currentQuestion = questions[i]
+// questionArray.push(questions[i])
+
+// }
+
+
+// }
+
+// startButton.addEventListener("click", startQuiz)
