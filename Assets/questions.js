ddee1480f0f17b22e59d2574e1888267b6091604
aimeemattson <amattson84@gmail.com>
2020-12-02 23:31:01 -0800

    deleting code and starting fresh

---
+++ Assets/questions.js
@@ -0,0 +1,46 @@
+var myQuestions = [
+    { 
+       question: "What is finding and fixing errors in programs ?", 
+       answers: { 
+       a:"Debugging",
+       b:"Program", 
+       c:"Bug", 
+       d:"Code"
+       },
+        correctAnswer:"a"
+    },
+    { 
+       question: "What is the action of doing something over and over again ?",     
+       answers: {
+            a:"Loop",
+            b:"Persistence",
+            c:"Program", 
+            d:"Code"
+           }, 
+           correctAnswer:"a"
+   },
+
+   { 
+       question: "A person who writes code and communicates instructions to a computer in known as a ?", 
+   
+       answers: { 
+           a:"Banker", 
+           b:"Coder", 
+           c:"Tester", 
+           d:"Programmer"
+       }, 
+       correctAnswer:"d"
+   },
+
+   { 
+       question: "The letter 'H' in HTML stands for ?", 
+   
+       answers: { 
+           a:"Heavy", 
+           b:"Hyper", 
+           c:"Holding", 
+           d:"Hollow"
+       }, 
+       correctAnswer:"b"
+   }
+];
