b5361c67965866fe7a34c3dc20edd7d9f5674518
aimeemattson <amattson84@gmail.com>
2020-12-02 19:08:12 -0800

    updating source link

--- coding quiz.js
+++ coding quiz.js
@@ -55,9 +55,8 @@ var submitButton = document.getElementById('submit');
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
 
        
-       function showQuestions(questions, quizContainer) {
-            
-                  
+       function showQuestions(questions, quizContainer){
+                              
         var output = [];
         var answers;
 
@@ -84,7 +83,7 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
 
                 // add this question and its answer to the output
 
-        output.push(
+         output.push(
             '<div class="question">' + questions[i].question + '</div>'
             + '<div class="answers">' + answers.join('') + '</div>'
             );
@@ -100,7 +99,7 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
 
 
          // start of ShowResults function//
-    function showResults(questions, quizContainer, resultsContainer){
+     function showResults(questions, quizContainer, resultsContainer){
         var answerContainers = quizContainer.querySelectorAll('.answers');
 
         var userAnswer = '';
@@ -115,7 +114,7 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
 
                 numCorrect++;
 
-                answerContainers[i].style.color = 'lightgreen';
+                answerContainers[i].style.color = 'darkgreen';
                 alert("That is correct!");
                 
             }
@@ -123,7 +122,6 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
                 answerContainers[i].style.color = 'red';
                 alert("wrong Answer");
             }
-
 
             resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
             
@@ -141,133 +139,6 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton) 
 
         showResults(questions, quizContainer, resultsContainer);
         
-    }
+    }   
     
-    
-    // End of function generateQuiz
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // questions for quiz
-// var questions = [
-//     { prompt: "What is finding and fixing errors in programs ?", a:"Debugging", b:"Program", c:"Bug", d:"Code", answer:"a"},
-//     { prompt: "What is the action of doing something over and over again ?", a:"Loop", b:"Persistence", c:"Program", d:"Code", answer:"a"},
-//     { prompt: "A person who writes code and communicates instructions to a computer in known as a ?", a:"Banker", b:"Coder", c:"Tester", d:"Programmer", answer:"d"},
-//     { prompt: "The letter 'H' in HTML stands for ?", a:"Heavy", b:"Hyper", c:"Holding", d:"Hollow", answer:"b"},
-// ];
-
-// var startButton = document.getElementById("startButton")
-// // container for button needed.
-// var questionABtn = document.createElement("button")
-// var questionBBtn = document.createElement("button")
-// var questionCBtn = document.createElement("button")
-// var questionDBtn = document.createElement("button")
-
-// questionABtn.classList="answerButtonClass"
-// questionBBtn.classList="answerButtonClass"
-// questionCBtn.classList="answerButtonClass"
-// questionDBtn.classList="answerButtonClass"
-
-// var questionArray = []
-// var currentQuestion 
-
-// // var i = 0;
-
-// var score = 0;
-
-
-
-// // function startQuiz() {
-//     for(var i =0; i < questions.length; i++) {
-//        var response = window.prompt(questions[i].prompt);
-//        if(response == questions[i].answer) {
-//          score++;
-//          alert("You are correct!");
-//        } else {
-//          alert("That is false.");
-//        }             
-//      }
-//     // }
-//     alert("You got " + score + "/" + questions.length)
-
-    
-
-
-
-
-// // function startQuiz() {
-// //     for (var i =0; i <questions.length; i++) {
-// //         var answer = confirm(questions[i].q);
-    
-// //         if (answer === true && questions[i].a === "true" ||
-// //         answer === false && questions[i].a === "false") {
-    
-// //             score++;
-// //             alert("You are correct!");            
-// //         } 
-// //         else {
-// //             alert("You are wrong!");
-// //         }  
-// //     }
-// // }
-
-
-// // function questionRenderer() {
-// // if(questionsArray.lenght >=questions.length) {
-// //     // end quiz function here
-// // }
-// // else{
-// //     currentQuestion = questions[i]
-// // questionArray.push(questions[i])
-
-// // }
-
-
-// // }
-
-// // startButton.addEventListener("click", startQuiz)
