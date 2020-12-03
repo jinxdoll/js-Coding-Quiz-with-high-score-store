54feb6df69b587e2c9e8c71eed8b18ad14a06bec
aimeemattson <amattson84@gmail.com>
2020-12-02 23:33:19 -0800

    created global variables

---
+++ Assets/script.js
@@ -0,0 +1,10 @@
+// Global Variables
+var viewHScoresBtnEl = document.querySelector("#viewHScores");
+var timerEl = document.querySelector("#timer");
+var score = 0;
+var currentQ = 0;
+var highScores = [];
+var interval;
+var timeGiven = 60;
+var secondsElapsed = 0;
+
